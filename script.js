5fb73c3b00cb0eddb0d9ea4215a5d721987cadc1
Salikur Islam <salikur6@gmail.com>
2024-03-22 01:51:52 +0600

    cetagory menu completed

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log('got it')
+
+
+// All Cetagory btn access ---------
+const allBtnId = document.getElementById('all-btn');
+const musicBtnId = document.getElementById('music-btn');
+const comedyBtnId = document.getElementById('comedy-btn');
+const drawingBtnId = document.getElementById('drawing-btn');
+
+
+const allBtn = ()=>{
+    allBtnId.classList.add('active');
+    musicBtnId.classList.remove('active')
+    comedyBtnId.classList.remove('active')
+    drawingBtnId.classList.remove('active')
+}
+
+const musicBtn=()=>{
+    allBtnId.classList.remove('active');
+    musicBtnId.classList.add('active')
+    comedyBtnId.classList.remove('active')
+    drawingBtnId.classList.remove('active')
+}
+
+const comedyBtn =()=>{
+    allBtnId.classList.remove('active');
+    musicBtnId.classList.remove('active')
+    comedyBtnId.classList.add('active')
+    drawingBtnId.classList.remove('active')
+}
+
+const drawingBtn = ()=>{
+    allBtnId.classList.remove('active');
+    musicBtnId.classList.remove('active')
+    comedyBtnId.classList.remove('active')
+    drawingBtnId.classList.add('active')
+}
+
+
+
+
+
+
